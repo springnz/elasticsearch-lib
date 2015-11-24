30ae13f3ffa6477f6bc5540fc3d9a63efe3616a8
Stephen Zoio <stephen@springdom.com>
2015-11-24 16:29:16 +1300

    Added a test case. Moved back to Play Json.

--- src/main/scala/springnz/elasticsearch/utils/HttpUtils.scala
+++ src/main/scala/springnz/elasticsearch/utils/HttpUtils.scala
@@ -2,22 +2,21 @@ package springnz.elasticsearch.utils
 
 import com.ning.http.client.Response
 import dispatch.{Http, Req}
-import org.json4s.jackson.JsonMethods._
-import org.json4s.{JValue, _}
+import play.api.libs.json.{Json, JsValue}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object HttpUtils {
 
-  case class ResponseResult(response: Response, json: JValue)
+  case class ResponseResult(response: Response, json: JsValue)
 
   implicit class ReqOps(request: Req) {
 
-    def processHttpRequest(implicit ec: ExecutionContext): Future[ResponseResult] = {
-      val requestAdded = request.addQueryParameter("wait_for_completion", "true")
+    def processHttpRequest(waitForComplete: Boolean = false)(implicit ec: ExecutionContext): Future[ResponseResult] = {
+      val requestAdded = if (waitForComplete) request.addQueryParameter("wait_for_completion", "true") else request
       Http(requestAdded).map { response =>
         val responseBody = response.getResponseBody
-        val responseJson: JValue = parse(responseBody)
+        val responseJson: JsValue = Json.parse(responseBody)
         ResponseResult(response = response, json = responseJson)
       }
     }
