39d58e863c3c9861d133dae0ca62d3ad89144a7d
Stephen Zoio <stephen@springdom.com>
2015-11-24 16:10:07 +1300

    Took out the blocking

--- src/main/scala/springnz/elasticsearch/utils/HttpUtils.scala
+++ src/main/scala/springnz/elasticsearch/utils/HttpUtils.scala
@@ -2,11 +2,10 @@ package springnz.elasticsearch.utils
 
 import com.ning.http.client.Response
 import dispatch.{Http, Req}
-import org.json4s.{JValue, _}
 import org.json4s.jackson.JsonMethods._
+import org.json4s.{JValue, _}
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
 
 object HttpUtils {
 
@@ -14,14 +13,13 @@ object HttpUtils {
 
   implicit class ReqOps(request: Req) {
 
-    def processHttpRequest(duration: Duration = 10 seconds): ResponseResult = {
+    def processHttpRequest(implicit ec: ExecutionContext): Future[ResponseResult] = {
       val requestAdded = request.addQueryParameter("wait_for_completion", "true")
-
-      import scala.concurrent.ExecutionContext.Implicits.global
-      val response: Response = Await.result(Http(requestAdded), duration)
-      val responseBody = response.getResponseBody
-      val responseJson: JValue = parse(responseBody)
-      ResponseResult(response = response, json = responseJson)
+      Http(requestAdded).map { response =>
+        val responseBody = response.getResponseBody
+        val responseJson: JValue = parse(responseBody)
+        ResponseResult(response = response, json = responseJson)
+      }
     }
   }
 }
